78f335083b34e6bc59cf391dde12b34581e6c8b2
Scott Christley <scott.christley@utsouthwestern.edu>
2017-10-20 08:21:30 -0500

    data has been flattened in mongo

--- ireceptor-node/api/controllers/samples.js
+++ ireceptor-node/api/controllers/samples.js
@@ -26,36 +26,6 @@ module.exports = {
     postSamples: postSamples
 };
 
-var extractRecords = function(records, uuid_name) {
-    var objIDs = [];
-    var objDict = {};
-    for (var i = 0; i < records.length; ++i) {
-	if (objIDs.indexOf(records[i][uuid_name]) < 0)
-	    objIDs.push(records[i][uuid_name]);
-
-	objDict[records[i]['vdjserver_uuid']] = records[i];
-    }
-
-    return [ objIDs, objDict ];
-}
-
-var flattenRecords = function(records, objDict, uuid_name) {
-    for (var i = 0; i < records.length; ++i) {
-	delete records[i]['_id'];
-	var cp = objDict[records[i][uuid_name]];
-	for (var p in cp) {
-	    if (p == '_id') continue;
-	    if (p == 'vdjserver_uuid') continue;
-	    if (p == 'filename_uuid') {
-		// do not overwrite an existing file uuid
-		if (!records[i][p] || records[i][p].length == 0) records[i][p] = cp[p];
-		continue;
-	    }
-	    records[i][p] = cp[p];
-	}
-    }
-}
-
 /*
   Functions in a127 controllers used for operations should take two parameters:
 
@@ -71,74 +41,27 @@ function postSamples(req, res) {
 
     MongoClient.connect(url, function(err, db) {
 	assert.equal(null, err);
-	console.log("Connected successfully to server");
+	console.log("Connected successfully to mongo");
 
 	var v1db = db.db('v1public');
-	var napCollection = v1db.collection('nucleicAcidProcessing');
-	var cpCollection = v1db.collection('cellProcessing');
 	var sampleCollection = v1db.collection('sample');
-	var subjectCollection = v1db.collection('subject');
-	var studyCollection = v1db.collection('study');
 
-	napCollection.find().toArray()
+	sampleCollection.find().toArray()
 	    .then(function(records) {
-		//console.log(napRecords);
-		//console.log(napRecords.length);
+		//console.log(records);
+		//console.log(records.length);
 
 		// start with nucleicAcidProcessing records
 		for (var i = 0; i < records.length; ++i) results.push(records[i]);
-		var objs = extractRecords(records, 'cell_processing_uuid');
-
-		// query associated cellProcessing records
-		var query = { vdjserver_uuid: { $in: objs[0] } };
-		return cpCollection.find(query).toArray();
-	    })
-	    .then(function(records) {
-		//console.log(records);
-
-		// extract and flatten
-		var objs = extractRecords(records, 'sample_uuid');
-		flattenRecords(results, objs[1], 'cell_processing_uuid');
-
-		// query associated sample records
-		var query = { vdjserver_uuid: { $in: objs[0] } };
-		return sampleCollection.find(query).toArray();
-	    })
-	    .then(function(records) {
-		//console.log(records);
-
-		// extract and flatten
-		var objs = extractRecords(records, 'subject_uuid');
-		flattenRecords(results, objs[1], 'sample_uuid');	
-
-		// query associated subject records
-		var query = { vdjserver_uuid: { $in: objs[0] } };
-		return subjectCollection.find(query).toArray();
-	    })
-	    .then(function(records) {
-		//console.log(records);
-
-	    	// extract and flatten
-		var objs = extractRecords(records, 'study_uuid');
-		flattenRecords(results, objs[1], 'subject_uuid');	
-
-		// query associated study records
-		var query = { vdjserver_uuid: { $in: objs[0] } };
-		return studyCollection.find(query).toArray();
-	    })
-	    .then(function(records) {
-		//console.log(records);
-
-	    	// extract and flatten
-		var objs = extractRecords(records, 'vdjserver_uuid');
-		flattenRecords(results, objs[1], 'study_uuid');	
 	    })
 	    .then(function() {
-		// clean up data types
+		//console.log(results);
+		// any data cleanup
 		for (var i = 0; i < results.length; ++i) {
 		    for (var p in results[i]) {
 			if (!results[i][p]) delete results[i][p];
 			else if ((typeof results[i][p] == 'string') && (results[i][p].length == 0)) delete results[i][p];
+			else if (p == '_id') delete results[i][p];
 		    }
 		}
 	    })
