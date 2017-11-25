26adc0054cb862d807af337d25b7a17db0d72b97
Richard Bruskiewich <richard@starinformatics.com>
2017-11-24 22:29:10 -0800

    Put number of sequence items returned back up to 50 (see if it works)

--- ireceptor-node/api/controllers/sequences.js
+++ ireceptor-node/api/controllers/sequences.js
@@ -169,7 +169,7 @@ var querySequenceSummary = function (req, res) {
 
                 //console.log("7. final query");
                 //return annCollection.find(query).limit(100).toArray(); // hundred sequences seems quite excessive?
-                return annCollection.find(query).limit(20).toArray();
+                return annCollection.find(query).limit(50).toArray();
             })
             .then(function (records) {
                 records.forEach(function (r) {
